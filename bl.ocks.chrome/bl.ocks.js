6dae3a45f5b843f15b0f20b4cbc419726bf2f19f
Jason Davies <jason@jasondavies.com>
2010-10-10 19:13:34 +0100

    Replace with Mike Bostock's version
    This removes the jQuery dependency and probably runs a bit quicker.
    Probably worth it as it iterates over all links on all pages.  It also
    uses a nice icon instead of a text-based link.

--- bl.ocks.chrome/bl.ocks.js
+++ bl.ocks.chrome/bl.ocks.js
@@ -1,13 +1,26 @@
-var gist_re = /^https?\:\/\/gist\.github\.com\/(\d*)/i,
-    rel_re = /^\/?(\d+)$/,
-    on_gist = gist_re.test(location.href);
-$('a').each(function() {
-  var href = $(this).attr('href'),
-      m = href.match(gist_re);
-  if (on_gist && !(m && m[1])) {
-    m = href.match(rel_re);
+var reGist = /^https?\:\/\/gist\.github\.com\/(\d*)/i,
+    reRel = /^\/?(\d+)$/,
+    gist = reGist.test(location.href),
+    anchors = document.querySelectorAll("a[href]"),
+    anchor,
+    image,
+    imageURL = chrome.extension.getURL("bl.ocks.png"),
+    i = -1,
+    n = anchors.length,
+    href,
+    match;
+while (++i < n) {
+  match = (href = (anchor = anchors[i]).getAttribute("href")).match(reGist);
+  if (gist && !(match && match[1])) match = href.match(reRel);
+  if (match && match[1]) {
+    anchor = anchor.appendChild(document.createElement("a"));
+    anchor.setAttribute("href", "http://bl.ocks.org/" + match[1]);
+    anchor.setAttribute("title", "View bl.ock #" + match[1] + ".");
+    anchor.style.position = "relative";
+    anchor.style.marginLeft = "2px";
+    anchor.style.marginRight = "18px";
+    image = anchor.appendChild(document.createElement("img"));
+    image.setAttribute("src", imageURL);
+    image.style.position = "absolute";
   }
-  if (m && m[1]) {
-    $(this).after(' <a href="http://bl.ocks.org/' + m[1] + '">[bl.ocks.org]</a>');
-  }
-});
+}
